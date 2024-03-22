df740868ddf37369d021c2cd28a4e168cf2282c3
Dave Garnar <dgarnar@gmail.com>
2024-03-22 14:43:20 +0000

    refactor script layout to clean up and allow re-use

---
+++ src/components/script/layout/cover-page.tsx
@@ -0,0 +1,53 @@
+import { ColourBlendTypeType } from "@/app/(site)/script/_script-context/script-submission-schema";
+import Image from "next/image";
+import backPattern from "public/back-pattern.png";
+import { ReactNode } from "react";
+import oldStandard from "./old-standard-loader";
+import oswaldPro from "./oswald-loader";
+
+interface CoverPageProps {
+    name: string;
+    primaryColour: string;
+    secondaryColour?: string;
+    colourBlendType?: ColourBlendTypeType;
+    children?: ReactNode;
+}
+
+const CoverPage = ({
+    name,
+    primaryColour,
+    secondaryColour,
+    colourBlendType,
+}: CoverPageProps) => {
+    return (
+        <div
+            style={{
+                background: secondaryColour
+                    ? `linear-gradient(${primaryColour}, ${secondaryColour})`
+                    : primaryColour,
+            }}
+            className={`relative flex h-full items-center justify-center overflow-hidden ${oswaldPro.variable} ${oldStandard.variable}`}
+        >
+            <Image
+                alt="background-texture"
+                src={backPattern}
+                style={{
+                    objectFit: "cover",
+                    mixBlendMode: colourBlendType
+                        ? colourBlendType
+                        : "difference",
+                }}
+                placeholder="blur"
+                quality={100}
+                sizes="100%"
+                fill
+                className=" opacity-60"
+            />
+            <span className="text-center font-title text-7xl text-amber-950 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,0.8)]">
+                {name}
+            </span>
+        </div>
+    );
+};
+
+export default CoverPage;
