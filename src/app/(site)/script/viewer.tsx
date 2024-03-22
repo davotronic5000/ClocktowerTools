ae6447a10a80456dbb61f323045b1280cdcb9870
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-03-22 13:16:17 +0000

    Add in two base routes

--- src/app/(site)/script/viewer.tsx
+++ src/app/(site)/script/viewer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ScriptDownloadButton from "@/components/generate-pdf/script-download-button";
 import ScriptLayout from "@/components/script/layout/script-layout";
 import { useScriptContext } from "./_script-context/use-script-context";
