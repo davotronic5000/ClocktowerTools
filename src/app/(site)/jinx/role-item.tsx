0c5d0586943517c6f6fbb48215fa5d6faa7ac322
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-06-07 16:57:01 +0100

    Mobile friendly jinx list
    Co-authored-by: Dave <davotronic5000@users.noreply.github.com>

---
+++ src/app/(site)/jinx/role-item.tsx
@@ -0,0 +1,49 @@
+import getBackupRoleImage from "@/components/images/get-role-image";
+import getRole from "@/components/json-upload/get-role";
+import { Heading } from "@/components/typography";
+import Image from "next/image";
+
+interface RoleItemProps {
+    id: string;
+    name: string;
+    description: string;
+    inverted?: boolean;
+    className?: string;
+}
+
+const RoleItem = ({
+    id,
+    name,
+    description,
+    inverted = false,
+    className = "",
+}: RoleItemProps) => {
+    return (
+        <div
+            className={`col-span-2 grid grid-cols-[50px_auto] gap-x-1 rounded-sm ${
+                inverted ? "bg-slate-400" : ""
+            } ${className}`}
+        >
+            <Image
+                alt={`role-${id}`}
+                src={getRole(id).image || getBackupRoleImage()}
+                width={50}
+                height={50}
+                style={{
+                    objectFit: "cover",
+                }}
+                quality={100}
+            />
+            <div
+                className={`p-2 pl-0 ${
+                    inverted ? "text-slate-900" : "text-slate-200"
+                }`}
+            >
+                <Heading as="h3">{name}</Heading>
+                <div className="leading-tight">{description}</div>
+            </div>
+        </div>
+    );
+};
+
+export default RoleItem;
