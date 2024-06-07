c69504823e58668221f5d6ac52b587476cdb62c8
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-06-07 13:34:50 +0100

    Make header mobile friendly

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,43 @@
+"use client";
 import { Icon } from "@/components/icon";
 import Link from "next/link";
-import { ComponentType } from "react";
+import { ComponentType, useState } from "react";
+import { ToggleButton } from "react-aria-components";
 import DarkBox from "../simple-styles/dark-box";
 import { Heading } from "../typography";
 import NavBar from "./nav-bar";
 
 const Header: ComponentType = () => {
+    const [navOpen, setNavOpen] = useState(false);
     return (
         <DarkBox className="z-10">
             <header
-                className={`flex justify-between border-b-2 border-solid border-gray-600 p-3 shadow-xl shadow-gray-900/50`}
+                className={`flex flex-col border-b-2 border-solid border-gray-600 shadow-xl shadow-gray-900/50`}
             >
-                <Link
-                    href="/"
-                    className="flex items-center font-serif text-3xl font-semibold small-caps"
-                >
-                    <Icon type="clocktower" className="text-amber-500" />
-                    <Heading as="h1">Clocktower Guru</Heading>
-                    <Icon type="death-book" className="pl-2 text-amber-500" />
-                </Link>
-                <NavBar />
+                <div className="flex flex-wrap justify-between p-2">
+                    <Link
+                        href="/"
+                        className="flex items-center justify-between font-serif text-3xl font-semibold small-caps"
+                    >
+                        <Icon type="clocktower" className="text-amber-500" />
+                        <Heading as="h1" className="sm:px-2">
+                            Clocktower Guru
+                        </Heading>
+                        <Icon
+                            type="death-book"
+                            className="pl-2 text-amber-500"
+                        />
+                    </Link>
+                    <ToggleButton
+                        isSelected={navOpen}
+                        onChange={setNavOpen}
+                        className="md:hidden"
+                    >
+                        <Icon type="bars-3" className="text-white" size="sm" />
+                    </ToggleButton>
+                    <NavBar className="hidden basis-full md:flex lg:basis-auto" />
+                </div>
+                {navOpen && <NavBar className="md:hidden" />}
             </header>
         </DarkBox>
     );
