4c146aa3a7146098b54131e979dc81f2d0bdd345
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-06-14 15:25:41 +0100

    Update border to match rest of layout

--- src/app/(site)/change-log.tsx
+++ src/app/(site)/change-log.tsx
@@ -18,7 +18,7 @@ const ChangelogModal = ({ children }: { children: ReactNode }) => {
                 className="fixed inset-0 z-40 bg-gray-950/30 backdrop-blur"
                 isDismissable
             >
-                <Modal className="fixed bottom-0 right-0 top-0 w-full overflow-auto border-l border-slate-950 bg-slate-800 p-2 shadow-xl shadow-gray-900 lg:w-[600px]">
+                <Modal className="fixed bottom-0 right-0 top-0 w-full overflow-auto border-l border-gray-600 bg-slate-800 p-2 shadow-xl shadow-gray-900 lg:w-[600px]">
                     <Dialog className="outline-none">
                         {({ close }) => (
                             <div className="flex flex-col">
