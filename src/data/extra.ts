fc8f48624f5e96b6ad6d2355a24c8735d09c0f04
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-06-14 16:40:47 +0100

    Add extra json details

---
+++ src/data/extra.ts
@@ -0,0 +1,30 @@
+const extra = {
+    dawn: {
+        id: "dawn",
+        firstNight: 1000,
+        firstNightReminder: "",
+        otherNight: 1000,
+        otherNightReminder: "",
+    },
+    dusk: {
+        id: "dusk",
+        firstNight: 0,
+        firstNightReminder: "",
+        otherNight: 0,
+        otherNightReminder: "",
+    },
+    minionInfo: {
+        id: "minionInfo",
+        firstNight: 5,
+        firstNightReminder:
+            "If more than one Minion, they all make eye contact with each other. Show the “This is the Demon” card. Point to the Demon.",
+    },
+    demonInfo: {
+        id: "demonInfo",
+        firstNight: 8,
+        firstNightReminder:
+            "Show the “These are your minions” card. Point to each Minion. Show the “These characters are not in play” card. Show 3 character tokens of good characters not in play.",
+    },
+};
+
+export default extra;
