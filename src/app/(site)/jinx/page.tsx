4411be286c90564823b2f1439f2033e4af01c223
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-07-05 17:25:27 +0100

    Add in search box frame

--- src/app/(site)/jinx/page.tsx
+++ src/app/(site)/jinx/page.tsx
@@ -24,10 +24,10 @@ const sortedJinxes = Object.keys(jinxes).sort((a, b) => {
 const Page = () => {
     return (
         <div>
-            <div></div>
             <div
                 className={`grid h-full w-full grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-2 p-2 sm:gap-4 sm:p-4`}
             >
+                <div className="p2 rounded bg-indigo-950">Hello</div>
                 {sortedJinxes.map((key) => {
                     const role = roles[key];
                     const jinxList = jinxes[key].jinx;
@@ -35,7 +35,7 @@ const Page = () => {
                     return (
                         <div
                             key={key}
-                            className="grid auto-rows-min grid-cols-2 rounded-lg bg-indigo-950 p-2"
+                            className="grid auto-rows-min grid-cols-2 rounded bg-indigo-950 p-2"
                             style={{
                                 gridRow: `span ${jinxSize} / span ${jinxSize}`,
                             }}
