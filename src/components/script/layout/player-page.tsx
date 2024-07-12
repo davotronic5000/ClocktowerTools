7b9fbcccc16c3c46564724d01f138d918eb1d586
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-07-12 11:33:04 +0100

    Adjust padding on sidebar

--- src/components/script/layout/player-page.tsx
+++ src/components/script/layout/player-page.tsx
@@ -28,7 +28,7 @@ const PlayerPage = ({ name, author, roles }: PlayerPageProps) => {
             <div className="z-0 col-span-full row-span-full row-start-2 grid grid-cols-[40px_1fr] grid-rows-[min-content_min-content_min-content_min-content] font-content">
                 {roleTypeOrder.map((roleType) => (
                     <Fragment key={roleType}>
-                        <div className="col-span-1 col-start-1 place-self-center py-4 text-base font-extrabold uppercase text-gray-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] vertical-writing-rl orientation-upright">
+                        <div className="col-span-1 col-start-1 place-self-center py-2 text-base font-extrabold uppercase text-gray-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] vertical-writing-rl orientation-upright">
                             {roleType}
                         </div>
                         <div
