f111db7d6aa10b5442c1e3bed026a143c25c4805
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-12-13 17:07:58 +0000

    fix missed value

--- src/components/json-upload/convert-script-to-json.ts
+++ src/components/json-upload/convert-script-to-json.ts
@@ -47,7 +47,7 @@ const convertScriptToJSON = async (scriptFile: File) => {
                     role: {
                         tokenSize: 170,
                         tokenMargin: 4,
-                        tokenImageBMargin: 10,
+                        tokenImageBMargin: 12,
                     },
                     reminder: {
                         tokenSize: 94.5,
